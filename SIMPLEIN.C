2c2142caaec7c4807417d69ed177ed6bc9a443fa
6pradeepthi <122279279+6pradeepthi@users.noreply.github.com>
2023-02-10 16:10:47 +0530

    Add files via upload

---
+++ SIMPLEIN.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+int main()
+{
+ int principle,rate,time,interest;
+ printf("enter the principle: ");
+ scanf("%d", &principle);
+ printf("enter the rate:");
+ scanf("%d", &rate);
+ printf("enter the time:");
+ scanf("%d", &time);
+ interest=principle*rate*time/100;
+ printf("the simple interest is %d",interest);
+ return 0;
+}
